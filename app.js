25be1f8d3448f59f13a2d356066554e23bd35c1c
pangbo <1838529223@qq.com>
2018-02-10 09:56:51 +0800

    index

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ mainRouter(app);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  next(err);
+  res.redirect('/');
+  //next(err);
 });
 
 // error handler
--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,8 @@
 module.exports = function(app){
 	app.get('/login',function(req,res){
 	  res.render('login')
+	});
+	app.get('/goodsList',function(req,res){
+		res.render('goods_list')
 	})
 }
