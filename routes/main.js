9499bd90d6235cbf9d196ce4b3861b9a095704ce
pangbo <1838529223@qq.com>
2018-02-10 13:53:42 +0800

    mall首页

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,9 @@ module.exports = function(app){
 	app.get('/login',function(req,res){
 	  res.render('login')
 	});
+	app.get('/mall',function(req,res){
+		res.render('index_mall')
+	})
 	app.get('/goodsList',function(req,res){
 		res.render('goods_list')
 	})
