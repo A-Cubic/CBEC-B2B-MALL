f84dbf30ff10495a69a8a138e4588f73f1bfb5b1
rainie_pang <1838529223@qq.com>
2018-02-13 11:36:37 +0800

    nav 模板

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,19 @@
+const fetch = require('node-fetch');
 module.exports = function(app){
 	app.get('/login',function(req,res){
 	  res.render('login')
 	});
 	app.get('/mall',function(req,res){
-		res.render('index_mall')
+		fetch('http://api.llwell.net/web/catalog', {
+			method: 'POST'
+		}) .then(res => res.json())
+			.then(json => {
+				console.log(json);
+				if(json.state == '0'){
+					res.render('index_mall',{results:json.results})
+				}
+			});
+
 	});
 	app.get('/goodsList',function(req,res){
 		res.render('goods_list')
